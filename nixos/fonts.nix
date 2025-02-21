19c1db7d572f4676a42ea76897637698d842bd99
zerone <913108060@qq.com>
2025-02-21 11:23:43 +0800

    update: font

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -2,21 +2,11 @@
 
   fonts = {
     packages = with pkgs; [
-	  roboto
-      work-sans
-      comfortaa
-      inter
-      lato
-      lexend
-      jost
-      nerd-fonts.meslo-lg
       noto-fonts
       noto-fonts-emoji
 	  noto-fonts-cjk-sans
 	  nerd-fonts.monaspace
 	  comic-mono
-	  openmoji-color
-      twemoji-color-font
       inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd
     ];
 
@@ -24,10 +14,10 @@
 
     fontconfig = {
       defaultFonts = {
-		monospace = [ "Monospace Nerd Font" "Noto Color Emoji" ];
-        sansSerif = [ "SFProDisplay Nerd Font" "Noto Color Emoji" ];
-        serif = [ "SFProDisplay Nerd Font" "Noto Color Emoji" ];
-        emoji = [ "Noto Color Emoji" ];
+		monospace = [ "Comic Mono" ];
+        # sansSerif = [ "SFProDisplay Nerd Font" "Noto Color Emoji" ];
+        # serif = [ "SFProDisplay Nerd Font" "Noto Color Emoji" ];
+        # emoji = [ "Noto Color Emoji" ];
       };
     };
   };
