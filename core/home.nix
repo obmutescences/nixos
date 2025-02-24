4e0ae37500b3e8dfac0192a78c50080a55d9d804
zerone <913108060@qq.com>
2025-02-24 15:26:36 +0800

    update: mime app

--- core/home.nix
+++ core/home.nix
@@ -27,6 +27,7 @@
     ../home/system/udiskie
     ../home/system/clipman
     ../home/system/fctix5
+	../home/system/mime
 	../themes/gtk.nix
 
     #./secrets # CHANGEME: You should probably remove this line, this is where I store my secrets
