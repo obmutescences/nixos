436397f3dd48e0710fd45b29b0fb862121dcc1be
zerone <913108060@qq.com>
2025-02-25 16:28:38 +0800

    update: dae read file

--- home/system/dae/default.nix
+++ home/system/dae/default.nix
@@ -1,12 +1,15 @@
+{ pkgs, ... }:
 {
   # 具体配置
   services.dae = {
     enable = true;
-    openFirewall = {
-        enable = false;
-        port = 12345;
-    };
-    configFile = "/etc/dae/config.dae";
+	disableTxChecksumIpGeneric = false;
+    # configFile = "/etc/dae/config.dae";
     # 其他参数...
+	config = builtins.readFile ./config1.dae;
+    assets = with pkgs; [
+      v2ray-geoip
+      v2ray-domain-list-community
+    ];
   };
 }
