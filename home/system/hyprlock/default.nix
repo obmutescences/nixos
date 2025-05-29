5dbc7f51bbac1e1a4afa3579ebd9711440f59de0
zerone <913108060@qq.com>
2025-05-29 15:39:14 +0800

    :recycle:  update: hyprlock config

--- home/system/hyprlock/default.nix
+++ home/system/hyprlock/default.nix
@@ -10,14 +10,14 @@ in {
     settings = {
       general = {
         grace = 5;
-        no_fade_in = false;
-        disable_loading_bar = false;
+        # no_fade_in = false;
+        # disable_loading_bar = false;
       };
 
       # BACKGROUND
       background = {
         monitor = "";
-        path = "/home/zerone/Pictures/Wallpapers/summer.jpg";
+        path = "/home/zerone/Pictures/Wallpapers/lock.png";
         blur_passes = 3;
         contrast = 0.8916;
         brightness = 0.7172;
@@ -53,10 +53,10 @@ in {
           monitor = "";
           text = "    $USER";
           color = foreground;
-          outline_thickness = 2;
-          dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
-          dots_spacing = 0.2; # Scale of dots' absolute size, 0.0 - 1.0
-          dots_center = true;
+          # outline_thickness = 2;
+          # dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
+          # dots_spacing = 0.2; # Scale of dots' absolute size, 0.0 - 1.0
+          # dots_center = true;
           font_size = 18;
           font_family = font + " Bold";
           position = "0, -180";
@@ -69,10 +69,10 @@ in {
       input-field = lib.mkForce {
         monitor = "";
         size = "300, 60";
-        outline_thickness = 2;
-        dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
-        dots_spacing = 0.2; # Scale of dots' absolute size, 0.0 - 1.0
-        dots_center = true;
+        # outline_thickness = 2;
+        # dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
+        # dots_spacing = 0.2; # Scale of dots' absolute size, 0.0 - 1.0
+        # dots_center = true;
         outer_color = "rgba(255, 255, 255, 0)";
         inner_color = "rgba(255, 255, 255, 0.1)";
         font_color = foreground;
