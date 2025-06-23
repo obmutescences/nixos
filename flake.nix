e4c20109c4a85a291f46a9115e6269896d70fa99
zerone <913108060@qq.com>
2025-06-23 11:58:25 +0800

    :wrench:  update: upgrade niri

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
     apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
     nur.url = "github:nix-community/NUR";
 	zen-browser.url = "github:0xc000022070/zen-browser-flake";
-	swww.url = "github:LGFae/swww/v0.10.3";
+	swww.url = "github:LGFae/swww";
 	# neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
 
 	rust-overlay = {
