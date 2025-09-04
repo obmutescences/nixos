05336eca693a2cf297f115e093b7a60c5f522d0c
zerone <913108060@qq.com>
2025-09-04 11:43:24 +0800

    :wrench:  update:  zen-browser, disable hypridel

--- home/system/hypridle/default.nix
+++ home/system/hypridle/default.nix
@@ -1,20 +1,19 @@
 # Hypridle is a daemon that listens for user activity and runs commands when the user is idle.
 { pkgs, ... }: {
   services.hypridle = {
-    enable = true;
+    enable = false;
     settings = {
-
       general = {
         ignore_dbus_inhibit = false;
-        lock_cmd = "pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "hyprctl dispatch dpms on";
+        # lock_cmd = "pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
+        # before_sleep_cmd = "loginctl lock-session";
+        # after_sleep_cmd = "hyprctl dispatch dpms on";
       };
 
       listener = [
         {
           timeout = 1800;
-          on-timeout = "pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
+          # on-timeout = "pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
         }
 
         # {
