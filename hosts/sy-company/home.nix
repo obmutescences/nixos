5b0749d306e2faed0c1d94bf71c7fcbfd32166d4
zerone <913108060@qq.com>
2026-03-30 14:27:45 +0800

    :wrench:  update: flake, system soft, home-manager version

--- hosts/sy-company/home.nix
+++ hosts/sy-company/home.nix
@@ -13,7 +13,7 @@
     file.".face.icon" = { source = ./profile_picture.png; };
 
     # Don't touch this
-    stateVersion = "24.05";
+    stateVersion = "26.05";
   };
 
   programs.home-manager.enable = true;
