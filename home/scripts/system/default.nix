f96c8288f2c121efcbbdcb0a356bb0c65dd655f2
zerone <913108060@qq.com>
2026-04-29 17:05:25 +0800

    :wrench:  update: add aicommits command

--- home/scripts/system/default.nix
+++ home/scripts/system/default.nix
@@ -90,5 +90,15 @@ let
     ''
 	flameshot gui -r | wl-copy -t image/png
     '';
+  aicommits-en = pkgs.writeShellScriptBin "aicommits-en"
+	# bash
+	''
+	aicommits -p "固定格式：:icon: type: message. 强制规则：1. :icon:：根据代码变更性质自动匹配标准 Emoji 短码（如 :sparkles: :bug: :memo: :art: :recycle: :zap: :white_check_mark: :rocket: :wrench: :rewind:）。2. type：必 须为全小写英文，且严格根据代码内容从以下列表中单选最匹配的一项：feat, fix, docs, style, refactor, perf, test, build, ci, chore, revert. 3. message：精准概括本次变更的核心目的。使用祈使句，末尾无标点。内容语言固定为英文. 4. 输出限制：仅输出符合上述格式的纯文本单行结果。严禁使用 Markdown 代码块、引号、前后缀、换行或任何分析解释。 示例：:sparkles: feat: add kuaishou campaign module, :bug: fix: prevent duplicate submission on weak network"
+	'';
+  aicommits-cn = pkgs.writeShellScriptBin "aicommits-cn"
+	# bash
+	''
+	aicommits -p "固定格式：:icon: type: message. 强制规则：1. :icon:：根据代码变更性质自动匹配标准 Emoji 短码（如 :sparkles: :bug: :memo: :art: :recycle: :zap: :white_check_mark: :rocket: :wrench: :rewind:）。2. type：必 须为全小写英文，且严格根据代码内容从以下列表中单选最匹配的一项：feat, fix, docs, style, refactor, perf, test, build, ci, chore, revert. 3. message：精准概括本次变更的核心目的。使用祈使句，末尾无标点。内容语言固定为中文. 4. 输出限制：仅输出符合上述格式的纯文本单行结果。严禁使用 Markdown 代码块、引号、前后缀、换行或任何分析解释。 示例：:sparkles: feat: add kuaishou campaign module, :bug: fix: prevent duplicate submission on weak network"
+	'';
 
-in { home.packages = [ menu powermenu lock start-neovide keyboard-sound-alpacas keyboard-sound-nkcream screenshot-pin ]; }
+in { home.packages = [ menu powermenu lock start-neovide keyboard-sound-alpacas keyboard-sound-nkcream screenshot-pin aicommits-en aicommits-cn]; }
